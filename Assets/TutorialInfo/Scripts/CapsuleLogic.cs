dd2190dab81e42afff8b62929599d3abed4541b8
UriosteguiM12 <marinauriostegui88@gmail.com>
2026-05-24 13:20:11 -0700

    added Spawn() and Despawn() to mole capsules

--- Assets/TutorialInfo/Scripts/CapsuleLogic.cs
+++ Assets/TutorialInfo/Scripts/CapsuleLogic.cs
@@ -10,28 +10,33 @@ public class CapsuleLogic : MonoBehaviour
     void Start()
     {
         capsuleRenderer = GetComponent<Renderer>();
+        StartCoroutine(Spawn());
     }
-    
-    // if clicked by the mouse, change thecolor of the capsule to red, wait a second, and then decrease it's Z value by whatever necessary to no longer be seenonthe board
 
     void OnMouseDown() {
-        Debug.Log("Object Clicked");
-        capsuleRenderer.material.color = new Color(0.5f, 1f, 5f);
-        StartCoroutine(WaitAndPrint());
-        
-        //for (int i = 0; i < 5; i++)
-        //{
-        //transform.Translate(new Vector3(0, -40f, 0) * Time.deltaTime);
-        //}
-        
+        StartCoroutine(Despawn());
     }
 
-    IEnumerator WaitAndPrint()
+    // Instead of immediately moving the Z to its desired place, this function does it incrementally, making it look "smoother"
+    // Runtime: a second
+    IEnumerator Despawn()
     {;
         for (int i = 0; i < 5; i++)
         {
-            yield return new WaitForSeconds(0.1f);
-            transform.Translate(new Vector3(0, -40f, 0) * Time.deltaTime);
+            yield return new WaitForSeconds(0.1f); // waits for a tenth of a second
+            transform.Translate(new Vector3(0, -0.2f, 0));
+        }
+        yield return new WaitForSeconds(2);
+        StartCoroutine(Spawn());
+    }
+
+    // Same as Despawn but in reverse 
+    IEnumerator Spawn()
+    {;
+        for (int i = 0; i < 5; i++)
+        {
+            yield return new WaitForSeconds(0.1f); // waits for a tenth of a second
+            transform.Translate(new Vector3(0, 0.2f, 0));
         }
     }
 }
