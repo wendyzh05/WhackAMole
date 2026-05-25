30e114c6ce84e9493eda284750296b70e29a6f1a
hjgong98 <hjgong@ucsc.edu>
2026-05-25 14:41:17 -0700

    menu stuff again

---
+++ Assets/TutorialInfo/Scripts/PauseMenuScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+// press escape to open menu (play open menu sound) (also pauses the game)
+// press resume game button to close menu (play a sound) (unpauses the game)
+// press quit button to exit game (play a sound)
+// press volume button to open volume panel (play a sound)
+// press credits button to open credits panel (play a sound)
+
+public class PauseMenuScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
