17665500244430a36c02dc0f0af34b6b20cfa211
hjgong98 <hjgong@ucsc.edu>
2026-05-27 23:12:01 -0700

    added a sound thing to start menu

--- Assets/TutorialInfo/Scripts/MenuManager.cs
+++ Assets/TutorialInfo/Scripts/MenuManager.cs
@@ -1,20 +1,56 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject creditsPanel;
+    
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip buttonClickSound;
+    [SerializeField] private AudioClip playGameSound;
+    [SerializeField] private AudioClip exitGameSound;
+    
+    private bool isCreditsVisible = false;
+    
+    private void Start()
+    {
+        if (creditsPanel != null)
+            creditsPanel.SetActive(false);
+        
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
+    
     public void PlayGame()
     {
-        SceneManager.LoadScene("Map Scene");
+        if (playGameSound != null && audioSource != null)
+            audioSource.PlayOneShot(playGameSound);
+        else if (buttonClickSound != null && audioSource != null)
+            audioSource.PlayOneShot(buttonClickSound);
+        
+        SceneManager.LoadScene("map scene");
     }
-
-    public void OpenCredits()
+    
+    public void ToggleCreditsPanel()
     {
-        // add this
+        if (buttonClickSound != null && audioSource != null)
+            audioSource.PlayOneShot(buttonClickSound);
+        
+        if (creditsPanel != null)
+        {
+            isCreditsVisible = !isCreditsVisible;
+            creditsPanel.SetActive(isCreditsVisible);
+        }
     }
     
     public void QuitGame()
     {
+        if (exitGameSound != null && audioSource != null)
+            audioSource.PlayOneShot(exitGameSound);
+        else if (buttonClickSound != null && audioSource != null)
+            audioSource.PlayOneShot(buttonClickSound);
+        
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.ExitPlaymode();
         #else
